2a998a4ffe58e337614a085d47ca537c974e35b0
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2018-08-22 18:43:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lexmark CX510 docker image
+
+## Other printers?
+
+May work with other Lexmark printers. CX410 maybe. Don't know.
---
+++ cups_sane.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Many things are hard-coded in this script
+#
+# If someone is interested in using it, even make it generic, I can help...
+# But for now, I am not even sure anyone would wanna use it.
+
+# Start cups
+/etc/init.d/dbus start
+/etc/init.d/avahi-daemon start
+/etc/init.d/cups start
+/etc/init.d/cups-browsed start
+
+# check if the printer is already configured
+lpstat -v 2>/dev/null | grep -q LexmarkCX510
+if [ $? -ne 0 ]
+then
+    # Printer driver is not installed: do it
+    printeruri=$(lpinfo -v | grep "Lexmark/CX510" | cut -d" " -f2)
+    if [ "$printeruri" = "" ]
+    then
+        echo "Lexmark CX510 printer not detected"
+    fi
+    # Add the printer
+    lpadmin -p LexmarkCX510 -E -v $printeruri -m lsb/usr/Lexmark_PPD/Lexmark_CX510_Series.ppd
+    # My options...
+    lpoptions -p LexmarkCX510 -o PageSize=A4
+fi
+
+
+# And finally, start SANE
+/usr/sbin/saned -d 2
